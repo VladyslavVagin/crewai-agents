504ec514ca151f5a33a1f03763e583d5374e357a
Vladyslav Vagin <vladtwince@gmail.com>
2025-07-29 19:19:59 +0200

    Debate Agent

---
+++ debate/src/debate/crew.py
@@ -0,0 +1,43 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+
+
+@CrewBase
+class Debate():
+    """Debate crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    @agent
+    def person(self) -> Agent:
+        return Agent(config=self.agents_config['person'], verbose=True)
+
+    @agent
+    def judge(self) -> Agent:
+        return Agent(config=self.agents_config['judge'], verbose=True)
+
+    @task
+    def propose(self) -> Task:
+        return Task(config=self.tasks_config['propose'])
+
+    @task
+    def oppose(self) -> Task:
+        return Task(config=self.tasks_config['oppose'])
+        
+    @task
+    def decision(self) -> Task:
+        return Task(config=self.tasks_config['decision'])
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Debate crew"""
+
+        return Crew(
+            agents=self.agents, # Automatically created by the @agent decorator
+            tasks=self.tasks, # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+        )
